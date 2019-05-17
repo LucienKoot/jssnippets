e990a026cc0bcd221f4eb84b7b122b6d4c4cabfe
Ludelaplu <lucien.koot@gmail.com>
2019-05-17 16:00:40 +0200

    overzicht van kleine oefeningen in console van browser met js

---
+++ jssnippets.js
@@ -0,0 +1,193 @@
+/*$(function() {
+
+let ten = 10;
+console.log(ten * ten);
+
+let mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
+
+let jansDebt = 150;
+jansDebt = jansDebt - 40;
+console.log(jansDebt);
+
+
+
+let lucienKrijgtvanMike = 400;
+lucienKrijgtvanMike = lucienKrijgtvanMike - 100;
+console.log(lucienKrijgtvanMike);
+
+var name = "HARRIET";
+const greeting = "HALLO ";
+console.log(greeting + name);
+
+let groet = "Hallo ", naam = "Joske";
+console.log(groet + naam);
+
+//onderstaand werken allemaal, maar even als comment gezet, anders gaan ze elkaar storen, maar ik wil de functies even bewaren.
+
+
+prompt("Enter PINcode \n to get your acces.");
+
+console.log(Math.max(66, 88));
+
+console.log(Math.min(44, 66) / 2);
+
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " +
+theNumber * theNumber);
+
+let the Number = Number(prompt("wilt u briefjes van: 10\n20\n50?"));
+console.log("Hier is uw te vele geld:" + theNumber * TheNumber);
+
+let num = Number(prompt("Pick a number"));
+if (num < 10) {
+console.log("Small");
+} else if (num < 100) {
+console.log("Medium");
+} else {
+console.log("Large");
+}
+
+let number = 0;
+while (number <=6887) {
+    console.log(number);
+    number = number + 14;
+}
+
+let yourName;
+do {
+    yourName = prompt("WIE ben jij?");
+} while (!yourName);
+console.log(yourName);
+
+
+
+if (false != true) {
+    console.log("lekker belangrijk");
+    if (3 < 4) {
+        console.log('dat is geen verassing');
+    }
+}
+
+
+switch (prompt ("hoe is het weer buiten?")) {
+    case "regen":
+    console.log("paraplu meenemen !!!");
+    break;
+    case "zonnig": 
+    console.log("ZWEMBROEK mee!!");
+    case "koud":
+    console.log("Muts op !!");
+    break;
+    default:
+    console.log("niet te voorspellen")
+    break;
+}
+*/
+/*Opdracht....
+
+Write a loop that makes seven calls to console.log to output the following
+triangle:
+#
+##
+###
+####
+#####
+######
+#######
+
+
+
+
+let number = 0;
+while (number <=6887) {
+    console.log(number);
+    number = number + 14;
+
+
+let number = 0 "#";
+while (number <=15) { 
+    console.log(number)
+    number = number + 1
+
+}
+
+
+const square = function(x) {
+    return x * x;
+    };
+    console.log(square(12));
+
+
+C
+
+
+document.getElementById("demo").innerHTML = "Even oefenen met innerHTML ";
+
+
+var x = 3
+var y = 48
+var z = x + y
+document.getElementById("demo").innerHTML = z;
+//
+
+
+
+
+
+
+if (leeftijd < 12)
+{
+toegangsprijs = 5
+duiken = false
+}
+else
+{
+toegangsprijs = 10
+duiken = true
+} 
+document.write
+
+function test()
+{
+    document.write("stukje tekst testen in functie")
+}
+test()
+*/
+// onderste laten staan i.v.m. laden pagina voordat script wordt uitgevoerd.
+
+});
+
+/*
+var demo = document.getElementById("demo");
+demo.innerHTML = .........;
+
+
+
+let lucienTegoed = 300;
+lucienTegoed = lucienTegoed - 200;
+var demo = document.getElementById("demo");
+demo.innerHTML = lucienTegoed;
+
+prompt ("hoe heet je?");
+"lucien"
+var username = prompt ("What is your name?")
+undefined
+username "Eva"
+
+VM460:1 Uncaught SyntaxError: Unexpected string
+var username = prompt("What is your name?");
+
+undefined
+username
+"Lucien"
+alert("Good job!");
+undefined
+alert (33*33);
+undefined
+
+
+
